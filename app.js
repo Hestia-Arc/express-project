50c0c1d5b5b4ac5dd5a45b88657a915095a682fb
Afolabi Esther <starafolab@gmail.com>
2024-04-23 05:04:00 +0100

    update app

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const https = require('https');
+const fs = require('fs'); 
+const os = require('os');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +24,61 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// --------NO 1 & 2
+
+https.get('https://en.wikipedia.org/wiki/Tunde_Onakoya', (response) => {
+  console.log('statusCode:', response.statusCode);
+  console.log('headers:', response.headers);
+
+  const fileData = fs.createWriteStream(path.join(__dirname, 'public', 'output.txt')); 
+
+  response.on('data', (data) => {
+    fileData.write(data); 
+  });
+
+  response.on('complete', () => {
+    console.log('Data completed')
+  })
+
+  response.on('close', () => { 
+    fileData.end(); 
+    console.log('Response closed');
+  });
+
+}).on('error', (error) => {
+  console.error(error);
+});
+
+// ---------- NO 3
+
+
+const filePath = path.join(__dirname, 'public', 'os-info.md'); 
+
+function formatUptime(uptime) {
+  const days = Math.floor(uptime / (60 * 60 * 24));
+  const hours = Math.floor((uptime % (60 * 60 * 24)) / (60 * 60));
+  const seconds = Math.floor((uptime % (60 * 60)) / 60);
+  return `${days}:${hours}:${seconds}`;
+}
+
+const content = `
+
+OS Arch: ${os.arch()}
+
+Uptime: ${formatUptime(os.uptime())}
+
+Home Dir: ${os.homedir()}\n`;
+
+fs.writeFile(filePath, content, 'utf8', (error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('OS info written');
+  }
+});
+
+// --------
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/os-info.md
@@ -0,0 +1,7 @@
+## System Information
+
+**OS Architecture:** x64
+
+**Uptime:** 28:4:40
+
+**Home Directory:** C:\Users\USER
