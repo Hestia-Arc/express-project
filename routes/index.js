1d2fa2836bfd435cad65996fe9d9705464d75c6e
Afolabi Esther <starafolab@gmail.com>
2024-05-09 12:21:14 +0100

    pug assignment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var fs = require("fs");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('agency', { title: 'Welcome!' });
+router.get("/", function (req, res, next) {
+  let navigationLinks = [
+    "Multiplex PCR",
+    "Technology",
+    "Design",
+    "Culture",
+    "Business",
+    "Sports",
+  ];
+
+  let rawdata = fs.readFileSync("./database/posts.json");
+
+  let posts = JSON.parse(rawdata);
+
+  let featuredPosts = posts.filter((item) => {
+    return item.is_featured === true;
+  });
+  let nonFeaturedPosts = posts.filter((item) => {
+    return item.is_featured === false;
+  });
+
+  res.render("blog", {
+    title: "Hestia's blog",
+    links: posts,
+    posts: [featuredPosts, nonFeaturedPosts],
+    
+  });
 });
 
 module.exports = router;
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,8 @@
 let mix = require("laravel-mix");
 
-mix.copy("node_modules/bootstrap/dist/css/bootstrap.css", "public/stylesheets/index.css")
+// mix.copy("node_modules/bootstrap/dist/css/bootstrap.css", "public/stylesheets/index.css")
+
+mix.sass('resources/sass/index.scss', 'public/stylesheets/app.css')
+    .js('resources/js/index.js', 'public/javascripts')
+    .copy('resources/sass/index.css', 'public/stylesheets/index2.css')
+    .copy('resources/sass/blog.css', 'public/stylesheets/');
